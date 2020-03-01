d1c3a2a2a0edf87d956a9401ce24f8a17a02a91d
ufengger <haifeng.wu@foxmail.com>
2020-03-01 21:03:29 +0800

    add master_doc in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -46,6 +46,8 @@ language = 'zh_CN'
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+master_doc = 'index'
+
 
 # -- Options for HTML output -------------------------------------------------
 
