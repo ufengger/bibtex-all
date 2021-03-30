677b389d8ba4476de7289c593c3572d3b1d99a39
ufengger <haifeng.wu@foxmail.com>
2021-03-30 17:04:34 +0800

    debug ppxm

--- scripts/ppxm.py
+++ scripts/ppxm.py
@@ -31,27 +31,22 @@ doc.save(xmpdf)
 doc.close()
 
 # crop each page
-crop(["-ap", "10", "-p", "5", xmpdf, "-o", xmcpdf])
+crop(["-b", "c", "-ap", "10", "-p", "5", xmpdf, "-o", xmcpdf])
 
 xmdoc = fitz.open(xmpdf)
 xmcdoc = fitz.open(xmcpdf)
 
 for pagenum in range(0, len(xmdoc), 3):
     xmpage = xmdoc.load_page(pagenum)
-    X0 = xmpage.mediabox.x0
-    Y0 = xmpage.mediabox.y0
-    X1 = xmpage.mediabox.x1
-    Y1 = xmpage.mediabox.y1
+    X0 = xmpage.cropbox.x0
+    Y0 = xmpage.cropbox.y0
+    X1 = xmpage.cropbox.x1
+    Y1 = xmpage.cropbox.y1
     xmcpage = xmcdoc.load_page(pagenum)
-    x0 = xmcpage.mediabox.x0
-    y0 = xmcpage.mediabox.y0
-    x1 = xmcpage.mediabox.x1
-    y1 = xmcpage.mediabox.y1
-
-    left_crop = x0
-    bott_crop = Y1 - y1
-    righ_crop = X1 - x1
-    topp_crop = y0
+    x0 = xmcpage.cropbox.x0
+    y0 = xmcpage.cropbox.y0
+    x1 = xmcpage.cropbox.x1
+    y1 = xmcpage.cropbox.y1
 
     width = x1 - x0
     height = y1 - y0
@@ -78,6 +73,3 @@ xmdoc.close()
 
 cmd = 'rm ' + xmpdf + ' ' + xmcpdf + ' ' + locfile
 os.system(cmd)
-
-# crop(["-ap4", str(left_crop), str(pu_bott_crop), str(righ_crop),
-#       str(topp_crop), "-g", str(pagenum + 1), "-p", "100", xmpdf, "-o", xmpdf])
