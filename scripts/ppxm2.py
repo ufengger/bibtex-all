f94954600e3dbf124fcdf5a2b8a1f18a07cbe686
ufengger <haifeng.wu@foxmail.com>
2021-04-10 20:23:26 +0800

    debug

--- scripts/ppxm2.py
+++ scripts/ppxm2.py
@@ -2,11 +2,12 @@ import os
 import fitz
 from pdfCropMargins import crop
 
-filename = '~/Sync/sci-tech/Boyd_convex_2004.pdf'
-outname = 'boyd_ch1_bipa.pdf'
+filename = '~/Sync/literature/Sandel_自由主义与正义的局限_2011.pdf'
+outname = 'liberty.pdf'
+lineH = 30.0
 
-chap_start = 81
-chap_end = 90
+chap_start = 7
+chap_end = 256
 
 # intermediate
 locfile = 'haha.pdf'
@@ -49,35 +50,43 @@ for pagenum in range(0, len(xmdoc), 2):
     width = x1 - x0
     height = y1 - y0
 
-    if height <= (1.4 * width):
-        # upper part
-        R = fitz.Rect(x0, y0, x1, y0 + 0.75 * width)
-        print("height less than 1.5w")
-        print(pagenum)
-        xmpage.set_cropbox(R)
+    # upper part
+    R = fitz.Rect(x0, y0, x1, y0 + 0.5 * height + lineH)
+    xmpage.set_cropbox(R)
+    # bottom part
+    xmpage = xmdoc.load_page(pagenum + 1)
+    R = fitz.Rect(x0, y0 + 0.5 * height - lineH, x1, y1)
+    xmpage.set_cropbox(R)
 
-        # bottom part
-        xmpage = xmdoc.load_page(pagenum + 1)
-        R = fitz.Rect(x0, y1 - 0.75 * width, x1, y1)
-        print("height less than 1.5w")
-        print(pagenum+1)
-        xmpage.set_cropbox(R)
-    else:
-        # upper part
-        R = fitz.Rect(x0, y0, x0 + 0.55 * 4 / 3 * height, y0 + 0.55 * height)
-        print("height greater than 1.5w")
-        print(pagenum)
-        xmpage.set_cropbox(R)
+    # if height <= (1.4 * width):
+    #     # upper part
+    #     R = fitz.Rect(x0, y0, x1, y0 + 0.75 * width)
+    #     print("height less than 1.5w")
+    #     print(pagenum)
+    #     xmpage.set_cropbox(R)
 
-        # bottom part
-        xmpage = xmdoc.load_page(pagenum + 1)
-        R = fitz.Rect(x0, y1 - 0.55 * height, x0 + 0.55 * 4 / 3 * height, y1)
-        print("height greater than 1.5w")
-        print(pagenum+1)
-        xmpage.set_cropbox(R)
+    #     # bottom part
+    #     xmpage = xmdoc.load_page(pagenum + 1)
+    #     R = fitz.Rect(x0, y1 - 0.75 * width, x1, y1)
+    #     print("height less than 1.5w")
+    #     print(pagenum+1)
+    #     xmpage.set_cropbox(R)
+    # else:
+    #     # upper part
+    #     R = fitz.Rect(x0, y0, x0 + 0.55 * 4 / 3 * height, y0 + 0.55 * height)
+    #     print("height greater than 1.5w")
+    #     print(pagenum)
+    #     xmpage.set_cropbox(R)
+
+    #     # bottom part
+    #     xmpage = xmdoc.load_page(pagenum + 1)
+    #     R = fitz.Rect(x0, y1 - 0.55 * height, x0 + 0.55 * 4 / 3 * height, y1)
+    #     print("height greater than 1.5w")
+    #     print(pagenum+1)
+    #     xmpage.set_cropbox(R)
 
 xmdoc.save(outname)
 xmdoc.close()
 
-cmd = 'rm ' + xmpdf + ' ' + xmcpdf + ' ' + locfile
-os.system(cmd)
+# cmd = 'rm ' + xmpdf + ' ' + xmcpdf + ' ' + locfile
+# os.system(cmd)
