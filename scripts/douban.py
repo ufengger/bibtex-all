28ea266d191465eb967f463c35f9aff61ea602ed
ufengger <haifeng.wu@foxmail.com>
2021-06-02 17:08:16 +0800

    add get_title_authors

--- scripts/douban.py
+++ scripts/douban.py
@@ -3,13 +3,26 @@
 import sys
 import requests
 import re
+import json
 from bs4 import BeautifulSoup
 
 headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0'}
 
+def get_authors(jdict, bib_dict):
+    """Get the authors from json dict."""
+    num = len(jdict['author'])
+    bib_dict['author'] = []
+    for k in range(num):
+        bib_dict['author'].append(jdict['author'][k]['name'])
+
 def get_title_authors(text, bib_dict):
-    """Get the title."""
-    return text
+    """Get the title and author(s)."""
+    pattern = '<script type="application/ld\+json">(.*?)</script>'
+    ldjson = re.search(pattern, text, re.S)
+    if ldjson != None:
+        jsondict = json.loads(ldjson.group(1))
+        bib_dict['title'] = jsondict['name']
+        get_authors(jsondict, bib_dict)
 
 def get_pub(text, bib_dict):
     """Get the publisher."""
@@ -23,6 +36,7 @@ def douban_entry(url, bib_dict):
     r = requests.get(url, headers=headers)
     if r.ok == True:
         get_pub(r.text, bib_dict)
+        get_title_authors(r.text, bib_dict)
         print(bib_dict)
     else:
         print('The url can not be requested.')
