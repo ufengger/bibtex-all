3aeb9c14efecce44764124ea201c12727835a813
ufengger <haifeng.wu@foxmail.com>
2021-06-18 14:51:52 +0800

    don't use remove spacing setup

--- scripts/epubzh2txt.sh
+++ scripts/epubzh2txt.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # convert Chinese epub to txt with fixed line width
-ebook-convert $1 $2 --force-max-line-length --max-line-length 38 --remove-paragraph-spacing
+ebook-convert $1 $2 --force-max-line-length --max-line-length 38
