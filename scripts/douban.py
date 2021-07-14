5cf17a0cfb7df5d2ca06c3b5ba4b511232b76ea6
ufengger <haifeng.wu@foxmail.com>
2021-07-14 16:30:34 +0800

    feat: 自动检测代理端口
    原来的代码中代理端口是固定的，现在添加了脚本自动从系统中读取代理端口。

--- scripts/douban.py
+++ scripts/douban.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
 
 import sys
+from os.path import expanduser
 from doubanspider import bibtex_export
+
 if len(sys.argv) < 4:
     proxies = {}
 else:
+    lantern_conf = expanduser("~") + '/.lantern/settings.yaml'
+    with open(lantern_conf, 'r') as file_l:
+        contents = file_l.readline()
+    addr = contents.split()[1]
     proxies = {
-        'http': '127.0.0.1:37013',
-        'https': '127.0.0.1:37013',
+        'http': addr,
+        'https': addr,
     }
 
 bibtex_export(sys.argv[1], sys.argv[2], proxies)
