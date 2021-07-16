6adf7cf8fea65962dd53536f9adba155bae1d336
ufengger <haifeng.wu@foxmail.com>
2021-07-16 12:09:33 +0800

    add proxy for v2ray

--- scripts/douban.py
+++ scripts/douban.py
@@ -7,13 +7,22 @@ from doubanspider import bibtex_export
 if len(sys.argv) < 4:
     proxies = {}
 else:
-    lantern_conf = expanduser("~") + '/.lantern/settings.yaml'
-    with open(lantern_conf, 'r') as file_l:
-        contents = file_l.readline()
-    addr = contents.split()[1]
-    proxies = {
-        'http': addr,
-        'https': addr,
-    }
+    if sys.argv[3] == 'l':
+        # lantern proxy
+        lantern_conf = expanduser("~") + '/.lantern/settings.yaml'
+        with open(lantern_conf, 'r') as file_l:
+            contents = file_l.readline()
+            addr = contents.split()[1]
+            proxies = {
+                'http': addr,
+                'https': addr,
+            }
+    elif sys.argv[3] == 'x':
+        # v2ray proxy
+        addr = '127.0.0.1:8889'
+        proxies = {
+            'http': addr,
+            'https': addr,
+        }
 
 bibtex_export(sys.argv[1], sys.argv[2], proxies)
